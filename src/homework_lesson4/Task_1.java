dcbc20cb05184a53b15b603e9e7f9f10fb7e65b9
Vinnichenko Yauheni <vinnichenkoE@tut.by>
2019-09-01 18:35:37 +0300

    lesson 4 task1

---
+++ src/homework_lesson4/Task_1.java
@@ -0,0 +1,12 @@
+package homework_lesson4;
+
+public class Task_1 {
+    public static void main(String[] args) {
+        for (int i=1; i<10;i++){
+            for (int j=1;j<10; j++){
+                System.out.print("" + i*j+"\t");
+            }
+            System.out.println("");
+        }
+    }
+}
